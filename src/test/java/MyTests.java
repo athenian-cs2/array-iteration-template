a71a0a29b9c8d9a0c04b9df7d345c57a802692d2
Nathan Lin <nlin@athenian.org>
2021-09-22 08:58:08 -0700

    update 2021

--- src/test/java/MyTests.java
+++ src/test/java/MyTests.java
@@ -22,15 +22,12 @@ public class MyTests {
         assertEquals(8, MyMain.secondLargest(new int[] {12, 2, 3, 4, 5, 7, 8}), "The second largest value in the array [12, 2, 3, 4, 5, 7, 8] should be: " + 8);    }
 
     @Test
-    public void testIsGeometric() {
-        assertEquals(true, MyMain.isGeometric(new int[] {1, 2, 4, 8, 16, 32}), "The output of whether the series [1, 2, 4, 8, 16, 32] is geometric should be: " + true);
-        assertEquals(false, MyMain.isGeometric(new int[] {1, 2, 4, 7, 14, 28}), "The output of whether the series [1, 2, 4, 7, 14, 28] is geometric should be: " + false);
-        assertEquals(false, MyMain.isGeometric(new int[] {1, 2, 4, 8, 16, 33}), "The output of whether the series [1, 2, 4, 8, 16, 33] is geometric should be: " + false);
-        assertEquals(true, MyMain.isGeometric(new int[] {1, 3, 9, 27, 81, 243, 729}), "The output of whether the series [1, 3, 9, 27, 81, 243, 729] is geometric should be: " + true);
-        assertEquals(false, MyMain.isGeometric(new int[] {1, 3, 9, 27, 81, 244, 729}), "The output of whether the series [1, 3, 9, 27, 81, 244, 729] is geometric should be: " + false);
-        assertEquals(true, MyMain.isGeometric(new int[] {1, -4, 16, -64, 256}), "The output of whether the series [1, -4, 16, -64, 256] is geometric should be: " + true);
-
-        assertEquals(true, MyMain.isGeometric(new int[] {32, 16, 8, 4, 2, 1}), "The output of whether the series [32, 16, 8, 4, 2, 1] is geometric should be: " + true);
-        assertEquals(true, MyMain.isGeometric(new int[] {256, 64, 16, 4, 1}), "The output of whether the series [256, 64, 16, 4, 1] is geometric should be: " + true);
+    public void testSpellCheck() {
+        assertEquals(true, MyMain.spellCheck("dog"), "The output of spellchecking \"dog\" should be: " + true);
+        assertEquals(false, MyMain.spellCheck("doge"), "The output of spellchecking \"doge\" should be: " + false);
+        assertEquals(true, MyMain.spellCheck("water"), "The output of spellchecking \"water\" should be: " + true);
+        assertEquals(true, MyMain.spellCheck("will"), "The output of spellchecking \"will\" should be: " + true);
+        assertEquals(false, MyMain.spellCheck("peoples"), "The output of spellchecking \"peoples\" should be: " + false);
+        assertEquals(false, MyMain.spellCheck("why"), "The output of spellchecking \"why\" should be: " + false);
     }
 }
